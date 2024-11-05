2337baecff27d73183b2cebf7af52a8e2b146542
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-05 12:31:14 +0000

    Implemented path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+      '@/assets': resolve('src/assets'),
+      '@/components': resolve('src/components'),
+      '@/constants': resolve('src/constants'),
+      '@/data': resolve('src/data'),
+      '@/types': resolve('src/types'),
+      '@/hooks': resolve('src/hooks'),
+      '@/styles': resolve('src/styles')
+    }
+  },
+  plugins: [react()]
+});
