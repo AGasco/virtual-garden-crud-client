fbc1c4343372497b9a998e524840ffff95e54d69
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-06 10:39:08 +0000

    Exported various folder contents from index

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as CreatePlant } from './CreatePlant';
+export { default as EditPlant } from './EditPlant';
+export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as NotFound } from './NotFound';
+export { default as PlantsList } from './PlantsList';
+export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as Register } from './Register';
